7ccc75fde2edfd3adc13333bbdbf11dafdc10baf
ezhicham <zghariservices@gmail.com>
2025-09-17 13:00:58 +0100

    setup the design of the project

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
-const express =require('express');
+const express = require("express");
 const app = express();
-const port =3000
-app.get("/",(req,res)=>{
-    res.sendFile("/views/home.html",{root:__dirname})
+const port = 3001;
+const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mydataSchema");
+app.set("view engine", "ejs");
+app.use(express.static('public'))
 
-})
+
+// Auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+ 
+ 
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+ 
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 
-app.listen(port,()=>{
-    console.log(`localhost:${port}/`)
-})
+
+
+
+
+
+app.get("/", (req, res) => {
+// result ==> array of objects
+res.render("index", {   });
+
+});
+
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add")
+});
+
+app.get("/user/view.html", (req, res) => {
+  res.render("user/view")
+});
+
+app.get("/user/edit.html", (req, res) => {
+  res.render("user/edit")
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://zghariservices_db_user:BaP9kwgdGHafiMYh@cluster0.vuexhvx.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
